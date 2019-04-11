4bffb7c26fcc55cb801d41395bde15f565d1cb5c
DanBlik <blikoff.d@ya.ru>
2019-04-11 14:51:15 +0300

    Добавил стили и фигуры

--- chess.js
+++ chess.js
@@ -3,6 +3,13 @@ function drawChess() {
     let block;
     let flag = true;
 
+    let figure = { //создаем массив с расположением фигур
+        0 : ['-263px -19px', '-484px -22px', '-372px -17px', '-150px -16px', '-38px -16px','-372px -17px', '-484px -22px','-263px -19px'],
+        1 : ['-595px -19px', '-595px -19px', '-595px -19px', '-595px -19px', '-595px -19px','-595px -19px', '-595px -19px','-595px -19px'],
+        6 : ['-595px -116px', '-595px -116px', '-595px -116px', '-595px -116px', '-595px -116px','-595px -116px', '-595px -116px','-595px -116px'],
+        7 : ['-263px -116px', '-484px -116px', '-372px -116px', '-38px -116px', '-150px -116px','-372px -116px', '-484px -116px','-263px -116px'],
+    };
+
     for (let i = 0; i < 8; i++){
         for (let j = 0; j < 8; j++){
             if (j == 0) flag = !flag;
@@ -10,6 +17,11 @@ function drawChess() {
 
             if (flag) block.className = 'block black';
             else block.className = 'block white';
+
+            if (figure[i]!==undefined && figure[i][j]!==undefined){
+                block.style.backgroundImage = 'url(chess.png)';
+                block.style.backgroundPosition = figure[i][j];
+            }
 
             mainBlock.appendChild(block);
             flag = !flag;
